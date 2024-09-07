de49eedc40bb3df4fc7b0ac53876647ca5d8e7fa
Bohdan <snizinkavolshebna@gmail.com>
2024-09-07 21:03:17 +0300

    sdsad

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,4 +1,5 @@
-import {lazy, Suspense, useEffect} from "react";
+import {useEffect} from "react";
+import '../css/login.css'
 import smallPrize from '../img/prizes-small-1.svg'
 import smallPrize2 from '../img/prizes-small-2.svg'
 import MainHeader from "../components/Header/MainHeader.jsx";
@@ -11,8 +12,6 @@ import Join from "./Index/Join.jsx";
 import Login from "./Index/Login.jsx";
 import Footer from "../components/Footer/Footer.jsx";
 import {Helmet} from "react-helmet";
-
-const Styling = lazy(() => import('../css/LoginAndBaseStyles.jsx'))
 export default function Index() {
     useEffect(() => {
         document.body.classList.forEach(item => document.body.classList.remove(item))
@@ -45,57 +44,53 @@ export default function Index() {
                     href="https://unpkg.com/accordion-js@3.3.4/dist/accordion.min.css"
                 />
             </Helmet>
-            <Suspense fallback={<>...</>}>
-                <Styling>
-                    <main>
-                        <Hero/>
-                        <div className="prizes">
-                            <div className="container">
-                                <div id="prizes" className="prizes__title">
-                                    Призы победителей конкурса
+            <MainHeader/>
+
+            <main>
+                <Hero/>
+                <div className="prizes">
+                    <div className="container">
+                        <div id="prizes" className="prizes__title">
+                            Призы победителей конкурса
+                        </div>
+                        <div className="prizes__flex">
+                            <div className="prizes__right-block">
+                                <div className="prizes__right-img">
+                                    <img src={smallPrize} alt=""/>
                                 </div>
-                                <div className="prizes__flex">
-                                    <div className="prizes__right-block">
-                                        <div className="prizes__right-img">
-                                            <img src={smallPrize} alt=""/>
-                                        </div>
-                                        <div className="prizes__left-text">
-                                            <div className="prizes__subtitle">
-                                                Вытупление на премии «СуперЛайкШоу-2024»
-                                            </div>
-                                            <p>
-                                                Победители исполнят трек детского мира и выступят
-                                                с ним на розовой дорожке Музыкальной Премии СТС Kids х Kids
-                                                Project
-                                            </p>
-                                        </div>
+                                <div className="prizes__left-text">
+                                    <div className="prizes__subtitle">
+                                        Вытупление на премии «СуперЛайкШоу-2024»
                                     </div>
-                                    <div className="prizes__right-block">
-                                        <div className="prizes__right-img">
-                                            <img src={smallPrize2} alt=""/>
-                                        </div>
-                                        <div className="prizes__left-text">
-                                            <div className="prizes__subtitle">Подарочный сертификат</div>
-                                            <p>
-                                                Каждый победитель получит подарочный сертификат на запись
-                                                своей песни в профессиональной студии звукозаписи
-                                            </p>
-                                        </div>
-                                    </div>
+                                    <p>
+                                        Победители исполнят трек детского мира и выступят
+                                        с ним на розовой дорожке Музыкальной Премии СТС Kids х Kids
+                                        Project
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="prizes__right-block">
+                                <div className="prizes__right-img">
+                                    <img src={smallPrize2} alt=""/>
+                                </div>
+                                <div className="prizes__left-text">
+                                    <div className="prizes__subtitle">Подарочный сертификат</div>
+                                    <p>
+                                        Каждый победитель получит подарочный сертификат на запись
+                                        своей песни в профессиональной студии звукозаписи
+                                    </p>
                                 </div>
                             </div>
                         </div>
-                        <Rules/>
-                        <Songs/>
-                        <Winners/>
-                        <FAQ/>
-                        <Join/>
-                        <Login/>
-                    </main>
-                </Styling>
-            </Suspense>
-            <MainHeader/>
-
+                    </div>
+                </div>
+                <Rules/>
+                <Songs/>
+                <Winners/>
+                <FAQ/>
+                <Join/>
+                <Login/>
+            </main>
 
             <Footer/>
         </>
