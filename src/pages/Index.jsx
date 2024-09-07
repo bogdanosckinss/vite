8e9eb5edad510945807bf66dd75c81bcf8efe328
Bohdan <snizinkavolshebna@gmail.com>
2024-09-07 21:02:40 +0300

    sdsad

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -46,55 +46,56 @@ export default function Index() {
                 />
             </Helmet>
             <Suspense fallback={<>...</>}>
-                <Styling />
+                <Styling>
+                    <main>
+                        <Hero/>
+                        <div className="prizes">
+                            <div className="container">
+                                <div id="prizes" className="prizes__title">
+                                    Призы победителей конкурса
+                                </div>
+                                <div className="prizes__flex">
+                                    <div className="prizes__right-block">
+                                        <div className="prizes__right-img">
+                                            <img src={smallPrize} alt=""/>
+                                        </div>
+                                        <div className="prizes__left-text">
+                                            <div className="prizes__subtitle">
+                                                Вытупление на премии «СуперЛайкШоу-2024»
+                                            </div>
+                                            <p>
+                                                Победители исполнят трек детского мира и выступят
+                                                с ним на розовой дорожке Музыкальной Премии СТС Kids х Kids
+                                                Project
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className="prizes__right-block">
+                                        <div className="prizes__right-img">
+                                            <img src={smallPrize2} alt=""/>
+                                        </div>
+                                        <div className="prizes__left-text">
+                                            <div className="prizes__subtitle">Подарочный сертификат</div>
+                                            <p>
+                                                Каждый победитель получит подарочный сертификат на запись
+                                                своей песни в профессиональной студии звукозаписи
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <Rules/>
+                        <Songs/>
+                        <Winners/>
+                        <FAQ/>
+                        <Join/>
+                        <Login/>
+                    </main>
+                </Styling>
             </Suspense>
             <MainHeader/>
 
-            <main>
-                <Hero/>
-                <div className="prizes">
-                    <div className="container">
-                        <div id="prizes" className="prizes__title">
-                            Призы победителей конкурса
-                        </div>
-                        <div className="prizes__flex">
-                            <div className="prizes__right-block">
-                                <div className="prizes__right-img">
-                                    <img src={smallPrize} alt=""/>
-                                </div>
-                                <div className="prizes__left-text">
-                                    <div className="prizes__subtitle">
-                                        Вытупление на премии «СуперЛайкШоу-2024»
-                                    </div>
-                                    <p>
-                                        Победители исполнят трек детского мира и выступят
-                                        с ним на розовой дорожке Музыкальной Премии СТС Kids х Kids
-                                        Project
-                                    </p>
-                                </div>
-                            </div>
-                            <div className="prizes__right-block">
-                                <div className="prizes__right-img">
-                                    <img src={smallPrize2} alt=""/>
-                                </div>
-                                <div className="prizes__left-text">
-                                    <div className="prizes__subtitle">Подарочный сертификат</div>
-                                    <p>
-                                        Каждый победитель получит подарочный сертификат на запись
-                                        своей песни в профессиональной студии звукозаписи
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <Rules/>
-                <Songs/>
-                <Winners/>
-                <FAQ/>
-                <Join/>
-                <Login/>
-            </main>
 
             <Footer/>
         </>
